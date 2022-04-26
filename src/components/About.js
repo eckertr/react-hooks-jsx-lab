a20080b2abe436b36410981bb7b1df587a000ac4
eckertr <rachel.eckert13@gmail.com>
2022-04-26 16:18:10 -0400

    commit tree 6c058446a0526da3e25ea7582c364be3a1e6c3f6

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,17 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">About
+  <h2>About Me</h2>
+  <p>I don't know what I am doing. I am hoping for the best.</p>
+  <img src={ image } 
+  alt = "I made this"/>
+  </div>;
 }
 
 export default About;
+
+//an `<h2>` element with the content of "About Me"
+  //- a `<p>` element with any content of your choosing
+  //- an `<img>` element with the `src` attribute set to the imported `image`
+    //variable, and the `alt` attribute set to "I made this"
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
 import React from "react";
-import { name, city } from "../data/data.js";
+import {name, city} from "../data/data.js";
 
-function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+const divStyle = {
+  color: "firebrick"
 }
+function Home() {
+  return <div id = "home"><h1 style={{color: "firebrick"}}>{name} is a Web Developer from {city}</h1>
+</div>}
 
 export default Home;
+
+//- a `<div>` element with an id of "home"
+//- inside the div element, therde should be one child element:
+//- an `<h1>` element, with the `style` attribute set to a color of "firebrick",
+  //and the content of "Your Name is a Web Developer from Your City"
+//- use JSX expression syntax to use the imported `name` and `city` variables
+  //inside your JSX eldements
